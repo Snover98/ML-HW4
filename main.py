34d30bff4c38dfdbc14b1a31e17a65587f8904b4
benbanuz <benbanuz@users.noreply.github.com>
2019-06-15 16:22:56 +0300

    did generative and general structure of main

--- main.py
+++ main.py
@@ -4,32 +4,32 @@ import pandas as pd
 import numpy as np
 
 
-def create_coalition(models: list, data: pd.DataFrame):
+def create_coalition(models: list, data: pd.DataFrame, threshold: int):
     coalitions = []
     for model in models:
         probs = model.predict_proba(data)
+        probs[probs < threshold] = 0
         con_mat = np.matmul(probs, np.transpose(probs))
         clustring_alg = sklearn.cluster.AgglomerativeClustering(n_clusters=2, connectivity=con_mat).fit(data)
         coalitions.append(clustring_alg.labels_)
     return coalitions
 
 
-def goodnes(coalitions: list):
-    pass
-
-
 def main():
     data = pd.read_csv("train_processed")
     X, Y = generative.target_features_split(data, "Vote")
+
     # asafs models
     asaf_models = []
 
     # generative_models
     gen_model = generative.train_generative(data)
 
-    coalitions = create_coalition(asaf_models + gen_model, X)
+    # can be updated later
+    threshold = 0.3
+    coalitions = create_coalition(asaf_models + gen_model, X, threshold)
 
     # check how good the coalitions are
     scores = []
     for coalition in coalitions:
-        scores.append(goodnes(coalition))
+        scores.append(sklearn.metrics.davies_bouldin_score(X, coalition))
